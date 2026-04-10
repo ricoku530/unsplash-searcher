5dc8731326330f6eddeb36d4b16760edac7435e0
ricoku530 <79895362@qq.com>
2026-04-10 13:13:26 +0800

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# Unsplash API 配置
-ACCESS_KEY = "BNmmGvLV735y2q7VnOElanEV8Tw03E0-JoVBtlJMr7M"
-BASE_URL = "https://api.unsplash.com"
-SEARCH_URL = f"{BASE_URL}/search/photos"
-PER_PAGE = 12  # 每页显示数量
