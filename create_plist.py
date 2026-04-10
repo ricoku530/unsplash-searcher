482d858ef3900adf405d89a5282085abcd76a10a
ricoku530 <79895362@qq.com>
2026-04-10 13:10:36 +0800

    Create Info.plist for UnsplashSearch app

---
+++ create_plist.py
@@ -0,0 +1,21 @@
+import os
+
+os.makedirs('dist/UnsplashSearch.app/Contents', exist_ok=True)
+
+plist = '''<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+<key>CFBundleName</key><string>UnsplashSearch</string>
+<key>CFBundleDisplayName</key><string>UnsplashSearch</string>
+<key>CFBundleIdentifier</key><string>com.unsplashsearch.app</string>
+<key>CFBundleVersion</key><string>1.0.0</string>
+<key>CFBundlePackageType</key><string>APPL</string>
+<key>CFBundleExecutable</key><string>UnsplashSearch</string>
+<key>LSMinimumSystemVersion</key><string>10.14</string>
+<key>NSHighResolutionCapable</key><true/>
+</dict>
+</plist>'''
+
+with open('dist/UnsplashSearch.app/Contents/Info.plist', 'w') as f:
+    f.write(plist)
